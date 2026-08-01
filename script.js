83622efa95fc1835b9f933a8c3cf05ab75435a94
itsliclara <itsliclara@gmail.com>
2026-08-01 16:40:58 +0400

    Countdown

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log("Hello World");
+const weddingDate = new Date("August 26, 2026 00:00:00").getTime();
+
+setInterval(function () {
+
+    const now = new Date().getTime();
+
+    const distance = weddingDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+    const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24))
+        /
+        (1000 * 60 * 60)
+    );
+
+    const minutes = Math.floor(
+        (distance % (1000 * 60 * 60))
+        /
+        (1000 * 60)
+    );
+
+    const seconds = Math.floor(
+        (distance % (1000 * 60))
+        /
+        1000
+    );
+
+    document.getElementById("days").textContent = days;
+
+    document.getElementById("hours").textContent = hours;
+
+    document.getElementById("minutes").textContent = minutes;
+
+    document.getElementById("seconds").textContent = seconds;
+
+},1000);
